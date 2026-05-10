40c249c11b4f9f9b27f0583d50aafe08defb6a0e
NurGeo <anzpro@gmail.com>
2026-05-10 14:26:49 +0500

    feat(course): Создать domain/types.ts (вспомогательные типы)

---
+++ packages/course/src/domain/types.ts
@@ -0,0 +1,16 @@
+/** Вспомогательные типы уровня модуля course */
+
+/** Идентификатор курса (UUID) */
+export type CourseId = string;
+
+/** Идентификатор урока (UUID) */
+export type LessonId = string;
+
+/** Идентификатор шага (UUID) */
+export type StepId = string;
+
+/** Идентификатор файла (UUID) */
+export type FileMetadataId = string;
+
+/** Порядковый номер (order) */
+export type Order = number;
