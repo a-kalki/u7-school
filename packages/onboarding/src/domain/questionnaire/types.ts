9af8b08df7a2ae5550d732364c529df9ec1c5976
NurGeo <anzpro@gmail.com>
2026-05-17 03:09:18 +0500

    refactor(onboarding): Сделать selectedAnswers опциональным в QuestionnaireActionResponse, добавить nextButton

--- packages/onboarding/src/domain/questionnaire/types.ts
+++ packages/onboarding/src/domain/questionnaire/types.ts
@@ -6,11 +6,13 @@ export type QuestionnaireActionResponse =
       type: 'wait_next';
       question: Question;
       selectedAnswers: string[];
+      /** Кнопка «Далее», формат "next:<questionCode>" */
+      nextButton?: string;
     }
   | {
       type: 'new_question';
       question: Question;
-      selectedAnswers: string[];
+      selectedAnswers?: string[];
     }
   | {
       type: 'completed';
