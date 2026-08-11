58fb3d4b50b4cb032adcfeeb2c5b709c67971dbe
NurGeo <anzpro@gmail.com>
2026-08-11 11:37:58 +0500

    feat(questionnaire): Создать структуру пакета questionnaire
    - package.json с зависимостями от core и user
    - tsconfig.json с extends от корневого
    - barrel exports: src/index.ts, src/domain/index.ts
    - paths в корневом tsconfig.json

---
+++ packages/questionnaire/src/domain/index.ts
@@ -0,0 +1,6 @@
+export * from './questionnaire/entity';
+export * from './questionnaire/question';
+export * from './questionnaire/types';
+export * from './questionnaire/question-pool-service';
+export * from './questionnaire/policy';
+export * from './questionnaire/repo';
